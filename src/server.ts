65c443a349623f505c69c91b9be095f2308af5e1
Henrique Ferraz <henriqueferraz@ofnet.com.br>
2025-03-11 10:35:35 -0300

    init

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import helmet from 'helmet';
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
